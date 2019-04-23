f054d2c85931c9297d700e4bfb7055e5d8ac450e
gary-baker <garybakerdev@gmail.com>
2019-04-23 21:50:56 +1200

    Post Class added and writeNewPost function added

--- app/src/main/java/me/garybaker/foodapp/MainActivity.java
+++ app/src/main/java/me/garybaker/foodapp/MainActivity.java
@@ -4,55 +4,51 @@ package me.garybaker.foodapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
 
-public class
-MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+    public class
+    MainActivity extends AppCompatActivity
+            implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String TAG = "PostDetailActivity";
+    private static final String TAG = "MainActivity";
 
 
-//    public FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    public DatabaseReference ref = database.getReference("https://foodapp-fa8fb.firebaseio.com/");
+    public FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public DatabaseReference ref = database.getReference("/");
 
 
     public FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-//    public String userID = mAuth.getCurrentUser().getUid();
 
 
 
@@ -63,6 +59,12 @@ MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button btnPost = (Button) findViewById(R.id.postData);
+        final EditText textPost = (EditText) findViewById(R.id.postText);
+
+
+
+
 
 
         mAuth.signInWithEmailAndPassword("garybakerdev@gmail.com", "Gary0704051").addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -71,17 +73,18 @@ MainActivity extends AppCompatActivity
                 if (task.isSuccessful()) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success");
+
+                    Toast.makeText(MainActivity.this, "Authentication Success.", Toast.LENGTH_SHORT).show();
                     FirebaseUser user = mAuth.getCurrentUser();
 
                     TextView hello = findViewById(R.id.hello);
 
-                    hello.setText((CharSequence) user);
+//                    hello.setText((CharSequence) user);
 
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithEmail:failure", task.getException());
-//                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
 
                 }
 
@@ -89,15 +92,17 @@ MainActivity extends AppCompatActivity
             }
         });
 
+        btnPost.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                textPost.getText();
 
 
-
-//        hello.setText("testing");
-
-//        hello.setText(mAuth.getCurrentUser().getUid());
-
-//        hello.setText("blah");
-//        ref.setValue("Hello, World!");
+                writeNewPost(mAuth.getUid(), mAuth.getCurrentUser().getDisplayName(), "blah", "yooozaaaa");
+            }
+        });
 
         FloatingActionButton fab;
         fab = findViewById(R.id.fab);
@@ -120,27 +125,6 @@ MainActivity extends AppCompatActivity
 
     }
 
-//    @Override
-//    public void onStart() {
-//
-//        super.onStart();
-//
-//
-//        // Attach a listener to read the data at our posts reference
-//        ref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                Post post = dataSnapshot.getValue(Post.class);
-//                System.out.println(post);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                System.out.println("The read failed: " + databaseError.getCode());
-//            }
-//        });
-//    }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -150,6 +134,22 @@ MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
+
+    private void writeNewPost(String userId, String username, String title, String body) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = ref.child("posts").push().getKey();
+        Post post = new Post(userId, username, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        ref.updateChildren(childUpdates);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
